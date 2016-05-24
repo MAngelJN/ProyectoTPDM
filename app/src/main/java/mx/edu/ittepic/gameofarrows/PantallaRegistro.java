6ae2d861d3123c0f1f6be25b1c799463f644cde3
Juan <jcarlostapia08@hotmail.com>
2016-05-24 16:22:32 -0600

    Pantalla Registro con imagen y texto

--- app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
@@ -1,13 +1,22 @@
 package mx.edu.ittepic.gameofarrows;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PantallaRegistro extends AppCompatActivity {
+
+    EditText nombre,alias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro);
+
+        nombre = (EditText) findViewById(R.id.editText3);
+        alias = (EditText) findViewById(R.id.fuente);
+        Typeface f = Typeface.createFromAsset(getAssets(),"GOT.ttf");
+        alias.setTypeface(f);
     }
 }
