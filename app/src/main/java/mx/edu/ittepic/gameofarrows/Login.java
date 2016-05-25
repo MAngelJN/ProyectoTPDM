2e87057b0ebd10c21c809af5d13af98a1b68b32c
Juan <jcarlostapia08@hotmail.com>
2016-05-24 19:33:18 -0600

    Login y registro

--- app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
@@ -1,13 +1,16 @@
 package mx.edu.ittepic.gameofarrows;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
     ImageView entrar,logo;
+    EditText alias, contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +18,17 @@ public class Login extends AppCompatActivity {
 
         logo = (ImageView) findViewById(R.id.imageView3);
         entrar = (ImageView) findViewById(R.id.imageView4);
+        alias = (EditText) findViewById(R.id.editText);
+        contra = (EditText) findViewById(R.id.editText2);
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirRegistro();
             }
         });
+        Typeface f = Typeface.createFromAsset(getAssets(),"GOT.ttf");
+        alias.setTypeface(f);
+        contra.setTypeface(f);
 
     }
 
