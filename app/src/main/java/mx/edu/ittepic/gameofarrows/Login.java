87f54edbbd9bb805244460dbfaeabf2081cafa27
Juan <jcarlostapia08@hotmail.com>
2016-05-24 18:59:28 -0600

    Pantalla login y registro

--- app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
@@ -1,13 +1,29 @@
 package mx.edu.ittepic.gameofarrows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
-    //algo mas bebe te amo :*
+    ImageView entrar,logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logo = (ImageView) findViewById(R.id.imageView3);
+        entrar = (ImageView) findViewById(R.id.imageView4);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirRegistro();
+            }
+        });
+
     }
+
+    public void abrirRegistro(){startActivity(new Intent(this,PantallaRegistro.class));}
+
 }
--- app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
@@ -4,10 +4,12 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class PantallaRegistro extends AppCompatActivity {
 
     EditText nombre,alias,cel,cel2;
+    ImageView img2,imgR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,8 @@ public class PantallaRegistro extends AppCompatActivity {
         alias = (EditText) findViewById(R.id.fuente);
         cel = (EditText) findViewById(R.id.editText4);
         cel2 = (EditText) findViewById(R.id.editText5);
+        img2 = (ImageView) findViewById(R.id.imageView);
+        imgR = (ImageView) findViewById(R.id.imageView2);
         Typeface f = Typeface.createFromAsset(getAssets(),"GOT.ttf");
         nombre.setTypeface(f);
         alias.setTypeface(f);
