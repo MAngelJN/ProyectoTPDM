650a559e06dfe07913a85e6b84ab90f699d1a968
Miguel Angel <MAngelJN@gmail.com>
2016-05-25 13:19:13 -0600

    No se que pedo

--- app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/Login.java
@@ -1,28 +1,51 @@
 package mx.edu.ittepic.gameofarrows;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-    //algo mas bebe te amo :*
-    Button button;
+    ImageView entrar,logo;
+    EditText alias, contra;
+    TextView registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //startActivity(new Intent(this, PantallaRegistro.class));
-        button = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        logo = (ImageView) findViewById(R.id.imageView3);
+        entrar = (ImageView) findViewById(R.id.imageView4);
+        alias = (EditText) findViewById(R.id.editText);
+        contra = (EditText) findViewById(R.id.editText2);
+        registro = (TextView) findViewById(R.id.textView);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this, PantallaRegistro.class));
+                abrirPrincipal();
             }
         });
 
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirRegistro();
+            }
+        });
+
+        Typeface f = Typeface.createFromAsset(getAssets(),"GOT.ttf");
+        alias.setTypeface(f);
+        contra.setTypeface(f);
+        registro.setTypeface(f);
+
     }
+
+    public void abrirRegistro(){startActivity(new Intent(this,PantallaRegistro.class));}
+    public void abrirPrincipal(){startActivity(new Intent(this,Principal.class));}
 
 }
--- app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
@@ -4,14 +4,28 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class PantallaRegistro extends AppCompatActivity {
 
+    EditText nombre,alias,cel,cel2;
+    ImageView img2,imgR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro);
 
+        nombre = (EditText) findViewById(R.id.editText3);
+        alias = (EditText) findViewById(R.id.fuente);
+        cel = (EditText) findViewById(R.id.editText4);
+        cel2 = (EditText) findViewById(R.id.editText5);
+        img2 = (ImageView) findViewById(R.id.imageView);
+        imgR = (ImageView) findViewById(R.id.imageView2);
+        Typeface f = Typeface.createFromAsset(getAssets(),"GOT.ttf");
+        nombre.setTypeface(f);
+        alias.setTypeface(f);
+        cel.setTypeface(f);
+        cel2.setTypeface(f);
     }
 }
