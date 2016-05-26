0bedebc11e90cd273dcad6bd2ee85ad0c99e2502
Miguel Angel <MAngelJN@gmail.com>
2016-05-26 00:09:17 -0600

    Verificacion del los campos antes del registro. Php a medias, faltan detalles

--- app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
+++ app/src/main/java/mx/edu/ittepic/gameofarrows/PantallaRegistro.java
@@ -11,11 +11,13 @@ import android.widget.ImageView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 public class PantallaRegistro extends AppCompatActivity {
 
     EditText nombre,alias,cel,cel2;
     ImageView img2,imgR;
+    String pW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,7 @@ public class PantallaRegistro extends AppCompatActivity {
         alias.setTypeface(f);
         cel.setTypeface(f);
         cel2.setTypeface(f);
+        pW = "";
 
         imgR.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,37 +43,58 @@ public class PantallaRegistro extends AppCompatActivity {
                 if (TextUtils.isEmpty(nombre.getText().toString())){
                     nombre.requestFocus();
                     nombre.setError("El nombre no puede ser vacio");
+                    return;
                 }
                 if (TextUtils.isEmpty(alias.getText().toString())){
                     alias.requestFocus();
                     alias.setError("El username no puede ser vacio");
+                    return;
                 }
                 if (TextUtils.isEmpty(cel.getText().toString())){
                     cel.requestFocus();
                     cel.setError("El Numero telefonico no puede ser vacio");
+                    return;
                 }
                 if (TextUtils.isEmpty(cel2.getText().toString())){
                     cel2.requestFocus();
                     cel2.setError("El Numero telefonico no puede ser vacio");
+                    return;
                 }
-                if(!cel.equals(cel2)){
+                if(cel.equals(cel2)){
+                    /*ConexionWeb conexionWeb = new ConexionWeb(PantallaRegistro.this);
+                    conexionWeb.agregarVariables("nombre",nombre.getText().toString());
+                    conexionWeb.agregarVariables("username", alias.getText().toString());
+                    conexionWeb.agregarVariables("cel", cel.getText().toString());
+
+                    generarPassword(6);
+
+                    conexionWeb.agregarVariables("contra", pW);
+
+                    try {
+                        URL url = new URL("http://gameofarrows.ueuo.com/GameOfArrows/registrar.php");
+                        conexionWeb.execute(url);
+                    }catch (MalformedURLException e){
+                        new AlertDialog.Builder(PantallaRegistro.this).setMessage(e.getMessage()).setTitle("Error").show();
+                    }*/
+                }else{
                     cel.requestFocus();
                     cel.setError("Los numeros telefonicos no coinciden");
                     cel2.setError("Los numeros telefonicos no coinciden");
                 }
 
-                /*if(cel.equals(cel2)){
-                        ConexionWeb conexionWeb = new ConexionWeb(Login.this);
-                        conexionWeb.agregarVariables("usuario",alias.getText().toString());
-                        conexionWeb.agregarVariables("contra",contra.getText().toString());
-                        try {
-                            URL url = new URL("http://gameofarrows.ueuo.com/GameOfArrows/login.php");
-                            conexionWeb.execute(url);
-                        }catch (MalformedURLException e){
-                            new AlertDialog.Builder(PantallaRegistro.this).setMessage(e.getMessage()).setTitle("Error").show();
-                        }
-                    }*/
             }
         });
+    }
+
+    private void generarPassword(int l){
+        long m = new java.util.GregorianCalendar().getTimeInMillis();
+        Random r = new Random(m);
+        char c;
+        for (int i=0;i<l;i++){
+            c = (char)r.nextInt(255);
+            if ((c >= '0' && c <= '9') || (c >='A' && c <='Z') ){
+                pW += c;
+            }
+        }
     }
 }
